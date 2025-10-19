43cb29279655fc817ae45c4ca489a035d762929f
Ian Woodward <woodwardian@proton.me>
2025-10-19 16:49:22 -0400

    added changes to entire site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      // Disable the default base styles since we're using DaisyUI
+      applyBaseStyles: false,
+    }),
+  ],
+});
